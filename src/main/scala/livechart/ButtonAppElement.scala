d087f33f042f8cb8424dc8cc9c88740f1b284722
Kevin Esler <kevin.esler@gmail.com>
2023-06-24 15:42:18 -0400

    Initialise

---
+++ src/main/scala/livechart/ButtonAppElement.scala
@@ -0,0 +1,35 @@
+package livechart
+
+import com.raquo.laminar.api.L.{*, given}
+
+object ButtonAppElement:
+  def appElement(): Element =
+    div(
+      a(href := "https://vitejs.dev", target := "_blank",
+        img(src := "/vite.svg", className := "logo", alt := "Vite logo"),
+      ),
+      a(href := "https://developer.mozilla.org/en-US/docs/Web/JavaScript", target := "_blank",
+        img(src := javascriptLogo, className := "logo vanilla", alt := "JavaScript logo"),
+      ),
+      h1("Hello Laminar!(from Kevo)"),
+      div(className := "card",
+        counterButton(),
+      ),
+      p(className := "read-the-docs",
+        "Click on the Vite logo to learn more",
+      ),
+    )
+  end appElement
+
+  private def counterButton(): Element =
+    val counter = Var(0)
+    button(
+      tpe := "button",
+      "count is ",
+      child.text <-- counter,
+      onClick --> { _ => counter.update(c => c + 1) },
+    )
+  end counterButton
+
+end ButtonAppElement
+
---
+++ src/main/scala/livechart/LiveChart.scala
@@ -0,0 +1,21 @@
+package livechart
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.*
+
+import org.scalajs.dom
+import com.raquo.laminar.api.L.{*, given}
+
+// import javascriptLogo from "/javascript.svg"
+@js.native @JSImport("/javascript.svg", JSImport.Default)
+val javascriptLogo: String = js.native
+
+@main
+def LiveChart(): Unit =
+  renderOnDomContentLoaded(
+    dom.document.getElementById("app"),
+    ButtonAppElement.appElement()
+  )  
+
+
+
